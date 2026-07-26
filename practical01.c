afb8304bfd12a1a9048447a681b512df149fede7
khinvasaradevam-maker <khinvasaradevam@gmail.com>
2026-07-26 19:30:24 +0530

    Create practical01.c

---
+++ practical01.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int num = 100;
+
+int main() {
+    int num = 50;
+
+    cout << "Local value: " << num << endl;
+    cout << "Global value: " << ::num << endl;
+
+    return 0;
+}
